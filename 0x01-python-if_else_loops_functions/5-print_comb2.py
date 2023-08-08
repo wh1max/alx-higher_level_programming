3f26ea54c920da1f3f1dace8e6600c3cf1a832cd
wh1max <oussama.zino56@gmail.com>
2023-08-08 16:20:50 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print("{}".format(num), end=", ")
+for num in range(0, 100):
+    if num == 99:
+        print(f"{num}")
+    else:
+        print("{}".format(num), end=", ")
