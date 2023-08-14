661f3df877ba4021c104a00faaca99871d87b4ce
wh1max <oussama.zino56@gmail.com>
2023-08-14 12:28:42 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if len(my_list) >= idx or idx < 0:
+        return my_list
+    else:
+        new_list = my_list[:]
+        new_list = element
+        return list_new
