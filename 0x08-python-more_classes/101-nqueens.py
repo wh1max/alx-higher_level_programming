446419dd3f8055885d44eaf504b0e996c6c785c8
wh1max <oussama.zino56@gmail.com>
2023-09-10 14:49:37 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,144 +1,89 @@
 #!/usr/bin/python3
-from sys import argv
-
-if len(argv) is not 2:
-    print('Usage: nqueens N')
-    exit(1)
-
-if not argv[1].isdigit():
-    print('N must be a number')
-    exit(1)
-
-N = int(argv[1])
-
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
+""" Placing N non-attacking queens on an NN chessboard. """
+import sys
 
 
-def board_column_gen(board=[]):
-    """Adds a column of zeroes to the right of any board about to be tested for
-    queen arrangements in that column.
-
-    Args:
-        board (list) of (list) of (int): 2D list of ints, only as wide as
-        needed to test the rightmost column for queen conflicts.
-
-    Returns:
-        modified 2D list
-
-    """
-    if len(board):
-        for row in board:
-            row.append(0)
-    else:
-        for row in range(N):
-            board.append([0])
-    return board
+matrix = []  # global variables
+board = []  # global variables
 
 
-def add_queen(board, row, col):
-    """Sets "queen," or 1, to coordinates given in board.
-
-    Args:
-        board (list) of (list) of (int): 2D list of ints, only as wide as
-            needed to test the rightmost column for queen conflicts.
-        row (int): first dimension index
-        col (int): second dimension index
-
-    """
-    board[row][col] = 1
+def result():
+    """ Prints result. """
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == 1:  # if a queen is met
+                matrix[row][1] = col  # matrix stores the column
+    return matrix
 
 
-def new_queen_safe(board, row, col):
-    """For the board given, checks that for a new queen placed in the rightmost
-    column, there are no other "queen"s, or 1 values, in the martix to the
-    left, and diagonally up-left and down-left.
+def backtrack(r, n, col, posDiag, negDiag):
+    """ Recursive function. """
+    global board
+    if r == n:  # end of row
+        print(result())  # print solution
+        return
 
-    Args:
-        board (list) of (list) of (int): 2D list of ints, only as wide as
-            needed to test the rightmost column for queen conflicts.
-        row (int): first dimension index
-        col (int): second dimension index
+    for c in range(n):
+        # checks if the queen can be in given column without
+        # attacking other queens, it also checks the positive
+        # and negative diagonals. If an attack is possible, it
+        # goes to the next column until the end of the row or
+        # there is no possible attack.
+        if c in col or (r + c) in posDiag or (r - c) in negDiag:
+            continue
 
-    Returns:
-        True if no left side conflicts found for new queen, or False if a
-    conflict is found.
+        # adds the row combinations to the set
+        col.add(c)
+        posDiag.add(r + c)
+        negDiag.add(r - c)
+        # board is updated to 1 to indicate that a queen
+        # is present in that position
+        board[r][c] = 1
 
-    """
-    x = row
-    y = col
+        # the function moves to the next row and runs again
+        backtrack(r + 1, n, col, posDiag, negDiag)
 
-    for i in range(1, N):
-        if (y - i) >= 0:
-            # check up-left diagonal
-            if (x - i) >= 0:
-                if board[x - i][y - i]:
-                    return False
-            # check left
-            if board[x][y - i]:
-                return False
-            # check down-left diagonal
-            if (x + i) < N:
-                if board[x + i][y - i]:
-                    return False
-    return True
+        # in the event that a queen can't be placed in any column,
+        # backtracking happens and the previous queen is moved
+        # hence the combinations of the previous queen need to be
+        # reset to 0
+        col.remove(c)
+        posDiag.remove(r + c)
+        negDiag.remove(r - c)
+        # board is updated to 0 to indicate that a queen
+        # is no longer in that position
+        board[r][c] = 0
 
 
-def coordinate_format(candidates):
-    """Converts a board (matrix of 1 and 0) into a series of row/column
-    indicies of each queen/1.
+def solve_n_queens(n):
+    """ Solve N Queens. """
+    col = set()
+    posDiag = set()
+    negDiag = set()
 
-    Args:
-    candidates (list) of (list) of (list) of (int): list of all successful
-        solutions for amount of columns last checked
+    global matrix
+    global board
+    matrix = [[c + r for c in range(2)] for r in range(n)]
+    board = [[0 for i in range(n)] for i in range(n)]
 
-    Attributes:
-        holberton (list) of (list) of (int): each member list contains the row
-    column number for each queen found
+    # call backtrack to place our queens
+    backtrack(0, n, col, posDiag, negDiag)
 
-    Returns:
-        holberton, the list of coordinates
 
-    """
-    holberton = []
-    for x, attempt in enumerate(candidates):
-        holberton.append([])
-        for i, row in enumerate(attempt):
-            holberton[x].append([])
-            for j, col in enumerate(row):
-                if col:
-                    holberton[x][i].append(i)
-                    holberton[x][i].append(j)
-    return holberton
+if len(sys.argv) != 2:  # wrong number of arguments
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-# init candidates list with first column of 0s
-candidates = []
-candidates.append(board_column_gen())
+num = sys.argv[1]
 
-# proceed column by column, testing the rightmost
-for col in range(N):
-    # start a new generation of the candidate list for every round of testing
-    new_candidates = []
-    # test each candidate from previous round, at current column
-    for matrix in candidates:
-        # for every row in that candidate's rightmost column
-        for row in range(N):
-            # are there any conflicts in placing a queen at those coordinates?
-            if new_queen_safe(matrix, row, col):
-                # no? then create a "child" (copy) of that candidate
-                temp = [line[:] for line in matrix]
-                # place a queen in that position
-                add_queen(temp, row, col)
-                # and unless you're in the last round of testing,
-                if col < N - 1:
-                    # add a new column of 0s on the right for the next round
-                    board_column_gen(temp)
-                # add that new candidate to this round's list of successes
-                new_candidates.append(temp)
-    # when finished with the round, discard the "parent" candidates
-    candidates = new_candidates
+try:
+    num_int = int(num)
+except ValueError:  # N must be an int
+    print("N must be a number")
+    sys.exit(1)
 
-# format results to match assignment output
-for item in coordinate_format(candidates):
-    print(item)
+if num_int < 4:  # N must be greater than or equal to 4
+    print("N must be at least 4")
+    sys.exit(1)
+
+solve_n_queens(num_int)
