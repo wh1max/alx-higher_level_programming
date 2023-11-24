0be9b6f816110c4c32eb3edac2b662166e50a80b
wh1max <oussama.zino56@gmail.com>
2023-11-24 07:09:33 -0800

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,6 +4,6 @@
 
 
 class Rectangle:
-        """Empty class per task instructions, will be built upon in later tasks.
-            """
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
     pass
